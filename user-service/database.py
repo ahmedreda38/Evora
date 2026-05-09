d6e65094f5222b5df2ae3a97ef80f3f718e9bf7d
ellen.freeman <ellen.freeman>
2026-05-09 15:37:26 +0000

    Added a docker network for consistent container communitcations

--- user-service/database.py
+++ user-service/database.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from os import getenv
 from dotenv import load_dotenv
 from pathlib import Path
-
+import os
 # Load .env from the current directory
 # env_path = Path(__file__).parent / ".env"
-# load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 # SQL_CONNECTION = getenv("DATABASE_URL")
 
-SQL_CONNECTION = "postgresql+psycopg2://db_admin:evora123@localhost:5432/evoradb"
+SQL_CONNECTION = os.environ['DATABASE_URL']
 
 if not SQL_CONNECTION:
     raise ValueError("DATABASE_URL environment variable is not set in .env file")
